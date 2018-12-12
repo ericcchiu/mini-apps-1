5420219b97506ba04dc3ca2a26d728e020487c17
ericcchiu <eric.chn.chiu@gmail.com>
2018-12-12 12:33:07 -0600

    (setup) Add babel6 to transpile react. Functioning setup, attempting to update to babel7.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,24 @@
+import React, {Component} from 'react'; 
+import ReactDOM from 'react-dom'; 
+
+class App extends Component { 
+  constructor(props) {
+    super(props); 
+    this.state = { 
+
+    }
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        <h1>Welcome to Multistep Checkout</h1>
+        
+      </div>
+    ) 
+  }
+}
 
 
+
+ReactDOM.render(<App />, document.getElementById('app')); 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,22 @@
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser'); 
+
+/*Serving static files*/
+app.use(express.static(path.join(__dirname, '/public'))); 
+
+/*Middleware */
+app.use(morgan('dev')); 
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json()); 
 
 
+/*Routes*/
+app.get('/', function(req, res) { 
+  res.status(200).send('Welcome to the homepage')
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}...`)); 
