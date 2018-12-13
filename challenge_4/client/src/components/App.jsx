33aab81889d7bb45c6ebffca7a793dc115c169c6
ericcchiu <eric.chn.chiu@gmail.com>
2018-12-13 10:56:50 -0600

    (setup) Complete initial setup and project folder organization.

---
+++ challenge_4/client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'; 
+
+
+class App extends Component {
+  constructor(props) { 
+    super(props); 
+
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>Welcome to Connect 5</h1>
+      </div>
+    )
+  }
+
+}
+
+export default App; 
+
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,16 @@
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser'); 
+const port = 3000; 
+const morgan = require('morgan'); 
+const path = require('path'); 
 
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev')); 
 
+// ROUTES
+
+app.listen(port, () => console.log(`Server listening on port ${port}...`));
